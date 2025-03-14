db556e01f8faff0ace6cbc102cdae131f869096b
abeg <abegodoy8998@gmail.com>
2025-03-14 00:54:44 -0500

    segundo commit

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import allRoutes from './routes/all.routes.js'
 
 const app=express()
 
+const cors = require('cors');
 app.set('port', config.port)
 app.use(express.json());
 
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended:false}));
 
+app.use(cors())
 //app.use(userRoutes);
 app.use(allRoutes);
 
